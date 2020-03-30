1328175ea74238cb011f282c098abe43e9edee3e
Sara <alu0100833010@ull.edu.es>
2020-03-30 19:57:41 +0100

    Reto Vinilo.

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -119,8 +119,8 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({"js/index.js":[function(require,module,exports) {
 /* index.js */
-console.log("Ready...");
-},{}],"../../../../.npm/_npx/3214/lib/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+console.log('Ready...');
+},{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -148,7 +148,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34179" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44485" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -324,5 +324,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../.npm/_npx/3214/lib/node_modules/parcel/src/builtins/hmr-runtime.js","js/index.js"], null)
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/index.js"], null)
 //# sourceMappingURL=/js.00a46daa.js.map
